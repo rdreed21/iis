a4844b33c23900de230f349b5cdfefe12552966f
rdreed21 <rdreed21@me.com>
2017-03-20 16:42:43 +0000

    Change

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,5 +1,7 @@
 powershell_script 'Install IIS' do
   code 'Add-WindowsFeature Web-Server'
+  guard_interpreter :powershell_script
+  not_if "(Get-WindowsFeature -Name Web-Server).Installed"
   action :run
 end
 
